5b0a062d4354aefa90a62486098a97f564a5370e
bruxo <diogo.stein@outlook.com.br>
2023-06-11 16:29:46 -0300

    Add a load spinner

--- js/script.js
+++ js/script.js
@@ -23,11 +23,13 @@ async function getUsers() {
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/users/?_limit=5'
     );
+    document.querySelector('.lds-spinner').classList.add('show');
 
     if (!response.ok) {
       throw new Error('HTTP error: ' + response.status);
     }
     const data = await response.json();
+    document.querySelector('.lds-spinner').classList.remove('show');
 
     addUsersToDOM(data);
   } catch (error) {
@@ -56,12 +58,14 @@ async function getPosts() {
     const response = await fetch(
       'https://jsonplaceholder.typicode.com/posts/?_limit=5'
     );
+    document.querySelector('.lds-spinner').classList.add('show');
 
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
 
     const data = await response.json();
+    document.querySelector('.lds-spinner').classList.remove('show');
 
     addPostsToDOM(data);
   } catch (error) {
@@ -92,12 +96,14 @@ const getPhotos = async () => {
   const response = await fetch(
     'https://api.slingacademy.com/v1/sample-data/photos'
   );
+  document.querySelector('.lds-spinner').classList.add('show');
 
   if (!response.ok) {
     throw new Error(`HTTP error: ${response.status}`);
   }
 
   const data = await response.json();
+  document.querySelector('.lds-spinner').classList.remove('show');
 
   addPhotosToDOM(data);
 };
